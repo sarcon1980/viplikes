3872d64334c1291e5355c9eea00cd11ebd814bc5
sarcon <saravelo.php.ukrtechinfo@gmail.com>
2023-05-15 18:07:05 +0300

    fix_options_column

---
+++ Modules/Service/Database/Migrations/2023_05_15_150412_fix_option.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('service_options', function (Blueprint $table) {
+
+            $table->string('title')->nullable()->change();
+            $table->string('types')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
